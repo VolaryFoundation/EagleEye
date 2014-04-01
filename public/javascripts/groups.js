b203c6a9ca90d3d85225b8592dc04397d9c2501f
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-04-01 13:27:11 -0700

    Have the basic of Backbone and rivets working

--- public/javascripts/groups.js
+++ public/javascripts/groups.js
@@ -1,13 +1,9 @@
-
 var Group = Backbone.Model.extend({
-  urlRoot: "http:///api/groups",
-  initialize: function() { }
+  urlRoot: "http://localhost:3000/groups"
 })
 
-var Groups = Backbone.Collection.extend({
-  url: '/api/groups',
-  model: Group
-})
+var group = new Group({id: theID});
+group.fetch()
 
 var UI = Backbone.Model.extend({
 
@@ -21,27 +17,52 @@ var UI = Backbone.Model.extend({
 })
 
 var ui = new UI
-var group = new Group
+
+rivets.adapters[':'] = {
+  subscribe: function(obj, keypath, callback) {
+    obj.on('change:' + keypath, callback)
+  },
+  unsubscribe: function(obj, keypath, callback) {
+    obj.off('change:' + keypath, callback)
+  },
+  read: function(obj, keypath) {
+    return obj.get(keypath)
+  },
+  publish: function(obj, keypath, value) {
+    obj.set(keypath, value)
+  }
+}
+
+rivets.configure({
+  handler: function(target, event, binding) {
+    this.call(binding.model, event, binding.view.models)
+  }
+})
+
+rivets.formatters.asAddress = function(val) {
+  if (!val) return ''
+  return val.address + '<br />' + val.city + ', ' + val.state
+}
 
 rivets.bind(document.getElementById('frame'), {
   group: group,
   ui: ui
 })
 
-new (Backbone.Router.extend({
-
-  routes: {
-    "groups/:id": "getGroup"
-  },
-
-  getGroup: function (id) {
-    group.set("id", id)
-    group.fetch().success(function() {
-      group.on('change', function() {
-        group.save(null, { patch: true, silent: true })
-      })
-    })
-  }
-}))
+//new (Backbone.Router.extend({
+//
+//  routes: {
+//    "groups/:id": "getGroup"
+//  },
+//
+//  getGroup: function (id) {
+//    group.set("id", id)
+//    group.fetch().success(function() {
+//      group.on('change', function() {
+//        group.save(null, { patch: true, silent: true })
+//      })
+//    })
+//  }
+//}))
 
 Backbone.history.start({ pushState: true })
