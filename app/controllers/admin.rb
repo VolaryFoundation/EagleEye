e6cac035569ab0504480e95b93b5b4a872e8d1b2
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-04-17 11:00:19 -0700

    Update admin panel to be admin only

--- app/controllers/admin.rb
+++ app/controllers/admin.rb
@@ -3,8 +3,12 @@ require APP_ROOT + "/models/claim"
 module SC
   class AdminController < BaseController
     get "/" do
-      @group_claims = Claim.all(:status => 'pending')
-      haml :"admin/index"
+      if current_user.present? && current_user.role == 'admin'
+        @group_claims = Claim.all(:status => 'pending')
+        haml :"admin/index"
+      else
+        redirect to("../")
+      end
     end
   end
 end
